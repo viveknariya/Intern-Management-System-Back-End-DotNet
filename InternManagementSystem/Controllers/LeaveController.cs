bc869f4b3f30a1d8deead8d749ca543265b46869
vivek <vivek@gmail.com>
2022-08-23 13:49:03 +0530

    leavecontroller and workinghourcntroller added

---
+++ InternManagementSystem/Controllers/LeaveController.cs
@@ -0,0 +1,55 @@
+﻿using InternManagementSystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveController : ControllerBase
+    {
+        private readonly InternContext _context = new InternContext();
+
+
+        [HttpGet]
+        public IActionResult LeaveList()
+        {
+            return Ok(_context.Leave);
+        }
+
+        [HttpPost]
+        public IActionResult AddRecord(Leave leave)
+        {
+            _context.Leave.Add(leave);
+            _context.SaveChanges();
+
+            return Ok(leave);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteRecord(string id)
+        {
+            var leave = _context.Leave.FirstOrDefault(l => l.InternId == id);
+            _context.Leave.Remove(leave);
+            _context.SaveChanges();
+
+            return Ok(leave);
+        }
+
+        [HttpPut]
+
+        public IActionResult PutRecord(Leave leave)
+        {
+            var L = _context.Leave.FirstOrDefault(l => l.InternId == leave.InternId);
+            L.LeaveDate = leave.LeaveDate;
+            L.Reason = leave.Reason;
+ 
+            return Ok(L);
+
+        }
+    }
+}
---
+++ InternManagementSystem/Controllers/WorkingHourController.cs
@@ -0,0 +1,55 @@
+﻿using InternManagementSystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkingHourController : ControllerBase
+    {
+        private readonly InternContext _context = new InternContext();
+
+
+        [HttpGet]
+        public IActionResult workingHourData()
+        {
+            return Ok(_context.WorkingHour);
+        }
+
+        [HttpPost]
+        public IActionResult AddRecord(WorkingHour wh)
+        {
+            _context.WorkingHour.Add(wh);
+            _context.SaveChanges();
+
+            return Ok(wh);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteRecord(string id)
+        {
+            var wh = _context.WorkingHour.FirstOrDefault(w => w.InternId == id);
+            _context.WorkingHour.Remove(wh);
+            _context.SaveChanges();
+
+            return Ok(wh);
+        }
+
+        [HttpPut]
+
+        public IActionResult PutRecord(WorkingHour wh)
+        {
+            var workingHour = _context.WorkingHour.FirstOrDefault(w => w.InternId == wh.InternId);
+            workingHour.CompanyWorkingHour = wh.CompanyWorkingHour;
+            workingHour.InternWorkingHour = wh.InternWorkingHour;
+
+            return Ok(workingHour);
+
+        }
+    }
+}
